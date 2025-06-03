3289423fb2d775cb76f2db03962c7242e2186eb3
James Frans Rizky Tambunan <139918042+jmsrzk14@users.noreply.github.com>
2025-06-04 00:15:04 +0700

    Update connection.go

--- API/database/connection.go
+++ API/database/connection.go
@@ -1,9 +1,11 @@
 package database
 
 import (
-	"api/models"
+	"fmt"
+	"os"
 	"time"
 
+	"api/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -11,43 +13,42 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	// Tambahkan parameter parseTime dan loc pada DSN
-	dsn := "root:@tcp(localhost:3306)/laravel?charset=utf8mb4&parseTime=True&loc=Local"
-	
-	// Tambahkan konfigurasi tambahan untuk GORM
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
 	conn, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		NowFunc: func() time.Time {
 			return time.Now().Local()
 		},
 	})
-	
+
 	if err != nil {
 		panic("could not connect to database: " + err.Error())
 	}
 
-	// Dapatkan koneksi database SQL underlying
 	sqlDB, err := conn.DB()
 	if err != nil {
 		panic("failed to get database instance: " + err.Error())
 	}
 
-	// Konfigurasi connection pool
 	sqlDB.SetMaxIdleConns(10)
 	sqlDB.SetMaxOpenConns(100)
 	sqlDB.SetConnMaxLifetime(time.Hour)
 
 	DB = conn
 
-	
-
-	// AutoMigrate dengan penanganan error
 	err = conn.AutoMigrate(
-		&models.Admin{}, &models.Category{}, &models.Product{}, 
+		&models.Admin{}, &models.Category{}, &models.Product{},
 		&models.User{}, &models.ProductDetail{}, &models.Review{},
-		&models.Wishlist{}, &models.Cart{}, &models.Order{}, 
+		&models.Wishlist{}, &models.Cart{}, &models.Order{},
 		&models.BuktiPembayaran{},
 	)
-	
+
 	if err != nil {
 		panic("failed to migrate database: " + err.Error())
 	}
